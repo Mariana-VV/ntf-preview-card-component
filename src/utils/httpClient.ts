e0c8ad45c18927f961b099515ee8f757ed896b1f
Mariana Trinko <marjanavv@gmail.com>
2025-08-31 09:44:58 +0200

    solution

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -1,5 +1,16 @@
 export function getData<Item>(): Promise<Item> {
-  return fetch("http://localhost:5173/api/data.json")
+  const headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "POST,PATCH,OPTIONS,GET",
+    "Access-Control-Allow-Credentials": "true",
+  };
+
+  return fetch("http://localhost:5173/api/data.json", {
+    mode: "no-cors",
+    method: "GET",
+    headers: headers,
+  })
     .then((res) => res.json())
     .then((data) => {
       return data;
